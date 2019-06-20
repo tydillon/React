c6660883211ee89f8f86f4e7fc12814a563ad4c0
Tyler Dillon <harrell_t@icloud.com>
2019-06-20 13:39:44 -0400

    010-adding-parcel

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ const App = () => {
       breed: 'Red Eared Slider'
     }),
     React.createElement(Pet, {
-      name: 'Fluffy',
+      name: 'Woof',
       animal: 'Rabbit',
       breed: 'Fuzzy'
     })
