9cb99d27c6f009151d3ca95943cf417e7e10b728
Tyler Dillon <harrell_t@icloud.com>
2019-06-20 14:06:04 -0400

    012-move-pet

--- src/app.js
+++ src/app.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, animal),
-    React.createElement('h2', {}, breed)
-  ]);
-};
+import Pet from './Pet';
 
 const App = () => {
   return React.createElement('div', {}, [
@@ -28,7 +21,7 @@ const App = () => {
       breed: 'Red Eared Slider'
     }),
     React.createElement(Pet, {
-      name: 'Woof',
+      name: 'Sniffles',
       animal: 'Rabbit',
       breed: 'Fuzzy'
     })
