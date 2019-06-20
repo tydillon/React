20ae65cd549a5c1ed8d45b69a15371433963a811
Tyler Dillon <harrell_t@icloud.com>
2019-06-20 14:23:25 -0400

    013-adding-jsx

--- src/app.js
+++ src/app.js
@@ -2,30 +2,42 @@ import React from 'react';
 import { render } from 'react-dom';
 import Pet from './Pet';
 
+// const App = () => {
+//   return React.createElement('div', {}, [
+//     React.createElement('h1', {}, 'Adopt Me!'),
+//     React.createElement(Pet, {
+//       name: 'Harley',
+//       animal: 'Dog',
+//       breed: 'Goldendoodle'
+//     }),
+//     React.createElement(Pet, {
+//       name: 'Chabela',
+//       animal: 'Dog',
+//       breed: 'Terrier'
+//     }),
+//     React.createElement(Pet, {
+//       name: 'Sally',
+//       animal: 'Turtle',
+//       breed: 'Red Eared Slider'
+//     }),
+//     React.createElement(Pet, {
+//       name: 'Sniffles',
+//       animal: 'Rabbit',
+//       breed: 'Fuzzy'
+//     })
+//   ]);
+// };
+
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(Pet, {
-      name: 'Harley',
-      animal: 'Dog',
-      breed: 'Goldendoodle'
-    }),
-    React.createElement(Pet, {
-      name: 'Chabela',
-      animal: 'Dog',
-      breed: 'Terrier'
-    }),
-    React.createElement(Pet, {
-      name: 'Sally',
-      animal: 'Turtle',
-      breed: 'Red Eared Slider'
-    }),
-    React.createElement(Pet, {
-      name: 'Sniffles',
-      animal: 'Rabbit',
-      breed: 'Fuzzy'
-    })
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Harley" animal="Dog" breed="Goldendoodle" />
+      <Pet name="Chabela" animal="Dog" breed="Terrier" />
+      <Pet name="Sally" animal="Turtle" breed="Red Eared Slider" />
+      <Pet name="Snookies" animal="Rabbit" breed="Fuzzy" />
+    </div>
+  );
 };
 
 render(React.createElement(App), document.getElementById('root'));
